db2b018257cd70a5762268351518a1062dffeb17
Rajat <rajatsingh52220@gmail.com>
2023-02-26 21:20:28 +0530

    Set color pallete

---
+++ src/common/config/colors.js
@@ -0,0 +1,16 @@
+export const neutral = {
+  900: "#000000",
+  800: "#222222",
+  A100: "#3C3E42",
+  A200: "#8D9098",
+  A300: "#F2F2F2",
+  A400: "#EDEDED",
+  A500: "#FFFFFF",
+  A600: "#F8F9FD",
+  A700: "#F9F9F9",
+  A800: "#EBEBED",
+};
+
+export const Blues = {
+  A100: "#2B87FE",
+};
--- src/common/config/theme.js
+++ src/common/config/theme.js
@@ -14,58 +14,6 @@ let theme = createTheme({
   status: {
     danger: "#ea1717",
   },
-  palette: {
-    primary: {
-      light: "#73BF7B",
-      main: "#44924C",
-      dark: "#2D6232",
-      contrastText: "#FFFFFF",
-    },
-    secondary: {
-      light: "#FFE7AD",
-      main: "#FFCE5C",
-      dark: "#664600",
-      contrastText: "#FFFFFF",
-    },
-    neutral: {
-      light: "#CCCCCC",
-      main: "#444444",
-      dark: "#000000",
-      contrastText: "#FFFFFF",
-    },
-    text: {
-      primary: "#444444",
-      secondary: "#666666",
-    },
-    error: {
-      "900": "#B20000",
-      "800": "#C11710",
-      "700": "#CD2018",
-      "600": "#DF2C1D",
-      "500": "#ED371C",
-      "400": "#EA493C",
-      "300": "#E36B64",
-      "200": "#EE948F",
-      "100": "#EE948F",
-      "50": "#FFEAEC",
-    },
-    tertiary: {
-      "900": "#003145",
-      "800": "#16465D",
-      "700": "#255973",
-      "600": "#346D89",
-      "500": "#417C9B",
-      "400": "#5D8FAC",
-      "300": "#76A4BF",
-      "200": "#98BFD7",
-      "100": "#B7DBEE",
-      "50": "#D9F1FF",
-      A100: "#002A3B",
-      A200: "#082835",
-      A400: "#002939",
-      contrastText: "#ffffff",
-    },
-  },
 
   spacing: 4,
 
@@ -160,29 +108,6 @@ let theme = createTheme({
     },
   },
   components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: 10,
-          textTransform: "none",
-          minWidth: "100px",
-          padding: "6px 18px",
-          outline: "none",
-          "&:focus": {
-            outline: "none",
-          },
-        },
-        sizeSmall: {
-          padding: "6px 12px",
-        },
-        outlinedPrimary: {
-          "&:hover": {
-            backgroundColor: "#44924C !important",
-            color: "#FFFFFF !important",
-          },
-        },
-      },
-    },
     MuiFormHelperText: {
       styleOverrides: {
         root: {
@@ -193,47 +118,5 @@ let theme = createTheme({
     },
   },
 });
-
-declare module "@mui/material/styles/createTheme" {
-  interface Theme {
-    status: {
-      danger: React.CSSProperties["color"];
-    };
-  }
-  interface ThemeOptions {
-    status: {
-      danger: React.CSSProperties["color"];
-    };
-  }
-}
-
-const colorOptions = {};
-declare module "@mui/material/styles/createPalette" {
-  type colorOptions = {
-    50: string;
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
-    A100: string;
-    A200: string;
-    A400: string;
-    A700: string;
-    contrastText: string;
-  };
-  interface Palette {
-    neutral: PaletteOptions["primary"];
-    tertiary: colorOptions;
-  }
-  interface PaletteOptions {
-    neutral: PaletteOptions["primary"];
-    tertiary: PaletteColorOptions;
-  }
-}
 
 export default theme;
