c983a819690e16980d3f4c74c926ab004611b1c8
Rajat <rajatsingh52220@gmail.com>
2023-03-24 14:25:17 +0530

    Update signin page

--- src/accounts/login/SignInForm.jsx
+++ src/accounts/login/SignInForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useSnackbar } from "notistack";
 import { Form, Formik } from "formik";
 import Box from "@mui/material/Box";
@@ -7,11 +8,16 @@ import Typography from "@mui/material/Typography";
 
 import SignInFormFields from "./SignInFormFields";
 import SubmitButton from "@/src/common/components/forms/SubmitButton";
+import AccountsService from "../service/AccountsService";
+import authService from "@/src/common/service/config/authService";
 import { Blues } from "@/src/common/config/colors";
 import { SignInFormValidationSchema } from "../utils/helper";
+import { FRONTEND_HOME_PAGE_URL } from "@/src/common/utils/constants";
 
+const accountsService = new AccountsService();
 const SignInForm = () => {
   const { enqueueSnackbar } = useSnackbar();
+  const router = useRouter();
 
   const initialState = {
     mail: "",
@@ -20,8 +26,33 @@ const SignInForm = () => {
 
   const handleSubmit = async (data, actions) => {
     const { mail, password } = data;
-    actions.setSubmitting(false);
-    // actions.resetForm();
+    const requestData = {
+      email: mail,
+      password,
+    };
+    accountsService
+      .userLogin(requestData)
+      .then((res) => {
+        console.log(res);
+        actions.setSubmitting(false);
+        enqueueSnackbar("User successfully Logged In", {
+          variant: "info",
+          autoHideDuration: 2000,
+          anchorOrigin: { horizontal: "right", vertical: "top" },
+        });
+        authService.setToken(res.data.token);
+        router.push(FRONTEND_HOME_PAGE_URL);
+      })
+      .catch((error) => {
+        actions.setSubmitting(false);
+        console.log(error);
+        enqueueSnackbar(error.response.data.message, {
+          variant: "error",
+          autoHideDuration: 2000,
+          anchorOrigin: { horizontal: "right", vertical: "top" },
+        });
+        actions.resetForm();
+      });
   };
 
   return (
