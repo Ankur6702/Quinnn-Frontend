9597f801e51bfb945c13f17588163a75cbb18a8c
Rajat <rajatsingh52220@gmail.com>
2023-03-31 23:55:22 +0530

    Create resources page

--- pages/resources/index.jsx
+++ pages/resources/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Head from "next/head";
+import Box from "@mui/material/Box";
+
+import CategoryCards from "@/src/resources/components/CategoryCards";
+import AnswerCard from "@/src/resources/components/AnswerCard";
+import TopicsCard from "@/src/resources/components/TopicsCard";
+import QuestionCards from "@/src/resources/components/QuestionCards";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <>
+      <CategoryCards />
+      <AnswerCard />
+      <TopicsCard />
+      {/* <QuestionCards /> */}
+    </>
+  );
 };
 
 export default index;
